3124edbf6e7741bb727605f699bbce4524967114
Ethan-OH <osm6227@gmail.com>
2023-10-23 14:59:46 +0900

    Restrict tab bar swipe function & add image picker package.

--- lib/view/tabbar_screen.dart
+++ lib/view/tabbar_screen.dart
@@ -9,12 +9,12 @@ class TabBarScreen extends StatefulWidget {
   const TabBarScreen({super.key});
 
   @override
-  State<TabBarScreen> createState() => _HomeState();
+  State<TabBarScreen> createState() => _TabBarScreenState();
 }
 
-class _HomeState extends State<TabBarScreen> with SingleTickerProviderStateMixin {
+class _TabBarScreenState extends State<TabBarScreen> with SingleTickerProviderStateMixin {
 
-  late TabController _tabController;
+  late TabController _tabController; // 탭바 컨트롤러
 
   @override
   void initState() {
@@ -37,6 +37,8 @@ class _HomeState extends State<TabBarScreen> with SingleTickerProviderStateMixin
     return Scaffold(
       body: TabBarView(
         controller: _tabController,
+        // 탭바 스와이프 제한
+        physics: const NeverScrollableScrollPhysics(),
         children: const [
           Home(),
           AgePrediction(),
@@ -48,7 +50,6 @@ class _HomeState extends State<TabBarScreen> with SingleTickerProviderStateMixin
         height: 70,
         color: Theme.of(context).colorScheme.onPrimary,
         child: TabBar(
-
           labelColor: Theme.of(context).colorScheme.primary,
           controller: _tabController,
           tabs: const [
