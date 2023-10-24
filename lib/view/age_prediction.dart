945d02f2440b7b495ea0041150c78ff5053a517c
Ethan-OH <osm6227@gmail.com>
2023-10-24 15:20:41 +0900

    연령대 퍼센트 위젯 생성, 퍼센트 인디케이터 패키지 추가

--- lib/view/age_prediction.dart
+++ lib/view/age_prediction.dart
@@ -1,7 +1,7 @@
 import 'package:final_main_project/styles/button_style.dart';
-import 'package:final_main_project/view/age_result.dart';
 import 'package:final_main_project/viewmodel/age_vm.dart';
 import 'package:final_main_project/widget/age/photo_area_widget.dart';
+import 'package:final_main_project/widget/age/result_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -84,49 +84,7 @@ class AgePrediction extends StatelessWidget {
             SizedBox(
               height: 30.h,
             ),
-            ElevatedButton(
-              style: secondaryButtonStyle(context),
-              onPressed: () {
-                Get.to(const AgeResultScreen());
-              },
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(5.w, 10.h, 5.w, 10.h),
-                child: Column(
-                  children: [
-                    const Text(
-                      '결과 확인하기',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 100.w,
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '30 ',
-                            style: TextStyle(
-                                fontSize: 17, fontWeight: FontWeight.bold),
-                          ),
-                          Icon(
-                            Icons.monetization_on,
-                            color: Colors.green,
-                            size: 25,
-                          ),
-                          Text(
-                            ' 차감',
-                            style: TextStyle(
-                                fontSize: 17, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            resultButtonWidget(context),
           ],
         ),
       ),
---
+++ lib/widget/age/age_percent_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+Widget buildCustomPercentIndicator(int age, double percent, Color color) {
+  return Padding(
+    padding: EdgeInsets.fromLTRB(5.w, 2.h, 5.w, 2.h),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(right: 5.w),
+          child: Text('$age대'),
+        ),
+        LinearPercentIndicator(
+          width: 200.w,
+          animation: true,
+          lineHeight: 20.0,
+          animationDuration: 2000,
+          percent: percent / 100,
+          center: Text("${(percent).toStringAsFixed(1)}%"),
+          progressColor: color,
+          barRadius: const Radius.circular(10),
+        ),
+      ],
+    ),
+  );
+}
