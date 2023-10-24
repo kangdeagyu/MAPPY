dd8b10f277a2904b5dc3941ce4f14989e1b71c77
kangdeagyu <rkdeorb0124@naver.com>
2023-10-24 23:54:55 +0900

    테마 변경 초기 작업

--- lib/view/more.dart
+++ lib/view/more.dart
@@ -1,4 +1,5 @@
 import 'package:final_main_project/components/more/charge_components.dart';
+import 'package:final_main_project/widget/more/etc_widget.dart';
 import 'package:final_main_project/widget/more/payment_widget.dart';
 import 'package:final_main_project/widget/more/userpage_widget.dart';
 import 'package:flutter/material.dart';
@@ -254,106 +255,13 @@ class More extends StatelessWidget {
                 ],
               ),
             ),
+            // 알람 끝
             const SizedBox(
               height: 30,
             ),
             // 기타
-            SizedBox(
-              width: 350,
-              height: 30,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 36,
-                    height: 36,
-                    clipBehavior: Clip.antiAlias,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/setting.png'),
-                      ),
-                    ),
-                  ),
-                  const Text(
-                    '기타',
-                    style: TextStyle(
-                      color: Color(0xFF2F9BFF),
-                      fontSize: 20,
-                      fontFamily: 'Lato',
-                      fontWeight: FontWeight.w700,
-                      height: 0,
-                    ),
-                  ),
-                ],
-              ),
-            ), // 기타 끝
-            const SizedBox(
-              height: 30,
-            ),
-            SizedBox(
-              width: 324,
-              height: 24,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const Text(
-                    'Dark Mode',
-                    style: TextStyle(
-                      color: Color(0xFF454545),
-                      fontSize: 20,
-                      fontFamily: 'Lato',
-                      fontWeight: FontWeight.w700,
-                      height: 0,
-                    ),
-                  ),
-                  const SizedBox(width: 150),
-                  SizedBox(
-                    width: 47,
-                    height: 23,
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          left: 0,
-                          top: 1,
-                          child: Container(
-                            width: 47,
-                            height: 21,
-                            decoration: ShapeDecoration(
-                              color: Color(0xFFF5F5F5),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.50),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          left: 24,
-                          top: 0,
-                          child: Container(
-                            width: 23,
-                            height: 23,
-                            decoration: const ShapeDecoration(
-                              color: Color(0xFF2F9BFF),
-                              shape: OvalBorder(),
-                              shadows: [
-                                BoxShadow(
-                                  color: Color(0x4F000000),
-                                  blurRadius: 3,
-                                  offset: Offset(0, 0),
-                                  spreadRadius: 0,
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            etcWidget(),
+            // 기타 끝
             const SizedBox(
               height: 30,
             ),
