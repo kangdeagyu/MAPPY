a7f9ff171c550d77f74a7c4ff680b2aeb5a7a9d0
kangdeagyu <rkdeorb0124@naver.com>
2023-10-25 16:53:29 +0900

    로그아웃 설정

--- lib/widget/more/logout_widget.dart
+++ lib/widget/more/logout_widget.dart
@@ -1,33 +1,41 @@
+import 'package:final_main_project/view/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 Widget logOut() {
-  return SizedBox(
-    width: 350,
-    height: 30,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Container(
-          width: 36,
-          height: 36,
-          clipBehavior: Clip.antiAlias,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/logout.png'),
+  return InkWell(
+    onTap: () {
+      Get.offAll(const LoginScreen());
+    },
+    child: SizedBox(
+      width: 350.w,
+      height: 30.w,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            width: 36.w,
+            height: 36.h,
+            clipBehavior: Clip.antiAlias,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/logout.png'),
+              ),
             ),
           ),
-        ),
-        const Text(
-          'Logout',
-          style: TextStyle(
-            color: Color(0xFF2F9BFF),
-            fontSize: 20,
-            fontFamily: 'Lato',
-            fontWeight: FontWeight.w700,
-            height: 0,
+          const Text(
+            'Logout',
+            style: TextStyle(
+              color: Color(0xFF2F9BFF),
+              fontSize: 20,
+              fontFamily: 'Lato',
+              fontWeight: FontWeight.w700,
+              height: 0,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
   );
 }
