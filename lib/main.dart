cb879ca0d677f1779245a3594ff35f1afb36a2f1
wookhyunKim <kwh1041@naver.com>
2023-10-30 12:06:47 +0900

    로그인 기능추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:final_main_project/view/login.dart';
+import 'package:final_main_project/view/firstpage.dart';
 import 'package:final_main_project/view/tabbar_screen.dart';
 import 'package:final_main_project/viewmodel/card_vm.dart';
 import 'package:final_main_project/viewmodel/theme_obs.dart';
@@ -54,7 +54,7 @@ class MyApp extends StatelessWidget {
               ),
               home: userid.uId.value.isNotEmpty
                   ? const TabBarScreen()
-                  : const LoginScreen(),
+                  : const First(),
             ));
       },
     );
---
+++ lib/view/firstpage.dart
@@ -0,0 +1,63 @@
+import 'package:final_main_project/view/loginpage.dart';
+import 'package:final_main_project/view/register.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class First extends StatelessWidget {
+  const First({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset(
+                        "assets/images/login.png",
+                        width: 340.w,
+                        height: 400.h,
+                        fit: BoxFit.fill, //이미지 가득채우기
+                      ),
+                      SizedBox(
+                        height: 100.h,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            Get.to(const LoginPage());
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              //  버튼 모양 깎기
+                              borderRadius: BorderRadius.circular(5), // 10은 파라미터
+                            ),
+                            minimumSize: const Size(300, 48),
+                          ),
+                          child: const Text(
+                            "로그인",
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10.h,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Get.to(const Register());
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              //  버튼 모양 깎기
+                              borderRadius: BorderRadius.circular(5), // 10은 파라미터
+                            ),
+                            minimumSize: const Size(300, 48),
+                          ),
+                          child: const Text(
+                            "회원가입",
+                          ),
+                        ),
+          ],
+        ),
+      ),
+    );
+  }
+}
